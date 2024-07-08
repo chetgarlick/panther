3f21785a4622c87e7d38350f53899e6b2aba00ac
Chet <chet.garlick@protonmail.com>
2024-07-08 14:58:45 -0500

    draw window

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,24 @@
 
 int main(){
 
+
+    sf::RenderWindow window(sf::VideoMode(200,200), "PANTHER");
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while(window.isOpen()){
+        sf::Event event;
+        while(window.pollEvent(event)){
+            if(event.type == sf::Event::Closed){
+                window.close();
+            }
+        }
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+
     return 0;
 };
