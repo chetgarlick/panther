c6bfbc6ee930e6bdddc125d1b5453cad665171d5
Chet <chet.garlick@protonmail.com>
2024-07-08 23:42:55 -0500

    use new player class

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include <SFML/Graphics.hpp>
 
 #include "Framerate/Framerate.h"
+#include "Player/Player.h"
 
 int main(){
 
     sf::RenderWindow window(sf::VideoMode(1920,1080), "PANTHER", sf::Style::Default);
     window.setFramerateLimit(360);
-    sf::CircleShape shape(100.f);
+    //sf::CircleShape shape(100.f);
+    Player player;
     Framerate frameRate;
+    player.Init();
+    player.Load();
     frameRate.Init();
     frameRate.Load();
     sf::Clock clock;
 
-    shape.setFillColor(sf::Color::Green);
+    //shape.setFillColor(sf::Color::Green);
 
     while(window.isOpen()){
         sf::Time deltaTimer = clock.restart();
@@ -25,10 +29,13 @@ int main(){
             }
         }
 
+        sf::Vector2f mousePos = sf::Vector2f(sf::Mouse::getPosition(window));
         frameRate.Update(delta);
+        player.Update(delta, mousePos);
         window.clear(sf::Color::Black);
         //window.draw(shape);
         frameRate.Draw(window);
+        player.Draw(window);
         window.display();
     }
 
