4b8c047aac5cef1c1c1221ff70299e8dd61f46a5
Chet <chet.garlick@protonmail.com>
2024-07-09 23:59:09 -0500

    remove unused again

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include <SFML/Graphics.hpp>
 #include "Game/Game.h"
-#include "Framerate/Framerate.h"
-#include "Player/Player.h"
 
 int main(){
 
     Game game;
     game.Launch();
-    
+
     return 0;
 };
