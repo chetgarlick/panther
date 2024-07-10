4874ba9342e1f5414bcd14b46d95936ebe42c67d
Chet <chet.garlick@protonmail.com>
2024-07-10 02:09:00 -0500

    use new button class for main menu buttons

--- src/MainMenu/MainMenu.h
+++ src/MainMenu/MainMenu.h
@@ -1,14 +1,16 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include "../Button/Button.h"
 
 class MainMenu{
     private:
         int selected;
         int initialTextVectLength;
         bool LRKeyHeld;
+        bool enterHeld;
         sf::Text menuText;
-        std::vector <sf::Text> textVect;
+        std::vector <Button> btnVect;
         sf::Font font;
 
     public:
