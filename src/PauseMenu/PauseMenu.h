eb0a4b7f5522eea38114acc826f9951639f233d9
Chet <chet.garlick@protonmail.com>
2024-11-18 12:59:29 -0600

    add pausemenu header file

---
+++ src/PauseMenu/PauseMenu.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <functional>
+#include "../Button/Button.h"
+
+
+class PauseMenu{
+    private:
+        int selected;
+        int initialTextVectLength;
+        bool LRKeyHeld;
+        bool enterHeld;
+        bool isPaused;
+        sf::Text menuText;
+        std::vector <Button> btnVect;
+        sf::Font font;
+
+    public:
+        PauseMenu();
+        ~PauseMenu();
+
+        void Init(std::function <void()> pauseFunc);
+        void Load();
+        void Update();
+        void HandlePlayerInput();
+        void Draw(sf::RenderWindow& window);
+};
